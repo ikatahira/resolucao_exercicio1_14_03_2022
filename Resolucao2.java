a0f6cee0eab7619926e5869246724e6b57d18b26
ikatahira <isaque.katahira@fatec.sp.gov.br>
2022-03-14 11:35:39 +0000

    exerc2

---
+++ Resolucao2.java
@@ -0,0 +1,18 @@
+
+public class Resolucao2{
+
+    public static void main(String[] args) {
+        
+        System.out.print("fdsddf");
+        for(int i=1;i<=4;i++){
+            if(i%2==0){
+            System.out.print(i);
+
+        }
+        else{
+            System.out.print(i);
+        }
+     
+}
+}
+}
